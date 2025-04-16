2166b96a228bc9382a60e8ce82c18954587e387d
Kim Daecheol <kdc9619@naver.com>
2025-04-16 13:22:13 +0900

    리포 생성

---
+++ src/main/java/com/ssginc/showpingrefactoring/ShowPingRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.ssginc.showpingrefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowPingRefactoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowPingRefactoringApplication.class, args);
+	}
+
+}
