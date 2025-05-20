fce38f71a87c1f50613333eb75b9a31f3775cc08
Kim Daecheol <kdc9619@naver.com>
2025-05-20 16:07:24 +0900

    Refactor: Watch API swagger 리팩토링 #38
    - Swagger 어노테이션을 Specification 인터페이스로 분리
    - 컨트롤러 클래스는 Specification 인터페이스의 구현체

--- src/main/java/com/ssginc/showpingrefactoring/domain/watch/controller/WatchApiController.java
+++ src/main/java/com/ssginc/showpingrefactoring/domain/watch/controller/WatchApiController.java
@@ -6,6 +6,7 @@ import com.ssginc.showpingrefactoring.domain.watch.dto.request.WatchRequestDto;
 import com.ssginc.showpingrefactoring.domain.watch.entity.Watch;
 import com.ssginc.showpingrefactoring.domain.watch.dto.response.WatchResponseDto;
 import com.ssginc.showpingrefactoring.domain.watch.service.WatchService;
+import com.ssginc.showpingrefactoring.domain.watch.swagger.WatchApiSpecification;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -30,11 +31,10 @@ import java.util.Map;
  * 영상 시청과 관련한 요청-응답을 수행하는 컨트롤러 클래스
  * <p>
  */
-@Tag(name = "watch", description = "시청 관련 API")
 @Controller
 @RequestMapping("/api/watch")
 @RequiredArgsConstructor
-public class WatchApiController {
+public class WatchApiController implements WatchApiSpecification {
 
     private final WatchService watchService;
 
@@ -45,25 +45,8 @@ public class WatchApiController {
      * @param userDetails 로그인한 사용자
      * @return 로그인한 사용자의 시청내역 응답 객체
      */
+    @Override
     @GetMapping("/history/list")
-    @Operation(
-            summary = "시청 내역 조회",
-            description = "로그인한 회원의 시청내역 조회"
-    )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "조회 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            array = @ArraySchema(
-                                    schema = @Schema(implementation = WatchResponseDto.class)
-                            )
-                    )
-            ),
-            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
-    })
-    @Parameter(hidden = true)
     public ResponseEntity<?> getWatchHistory(@AuthenticationPrincipal UserDetails userDetails) {
         // 로그인한 사용자의 정보를 가져오기
         Member member = memberService.findMemberById(userDetails.getUsername());
@@ -80,36 +63,8 @@ public class WatchApiController {
      * @param watchRequestDto 시청내역 등록을 위한 요청 DTO (Body를 통해 전달)
      * @return 응답 결과
      */
+    @Override
     @PostMapping("/insert")
-    @Operation(
-            summary = "시청 내역 조회",
-            description = "로그인한 회원의 시청내역 조회"
-    )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "조회 성공",
-                    content = @Content(
-                            mediaType = "application/json",
-                            array = @ArraySchema(
-                                    schema = @Schema(implementation = WatchResponseDto.class)
-                            )
-                    )
-            ),
-            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
-    })
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(
-            description = "추가할 시청 정보를 담은 JSON",
-            required    = true,
-            content     = @Content(
-                    mediaType = "application/json",
-                    schema    = @Schema(
-                            type        = "object",
-                            description = "추가할 시청 정보 예시",
-                            example     = "{\"streamNo\":\"1\", \"watchTime\":\"2025-01-01T12:00:00\"}"
-                    )
-            )
-    )
     public ResponseEntity<?> insertWatchHistory(@AuthenticationPrincipal UserDetails userDetails,
                                                 @RequestBody WatchRequestDto watchRequestDto) {
         Long memberNo = null;
---
+++ src/main/java/com/ssginc/showpingrefactoring/domain/watch/swagger/WatchApiSpecification.java
@@ -0,0 +1,73 @@
+package com.ssginc.showpingrefactoring.domain.watch.swagger;
+
+import com.ssginc.showpingrefactoring.domain.watch.dto.request.WatchRequestDto;
+import com.ssginc.showpingrefactoring.domain.watch.dto.response.WatchResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "watch", description = "시청 관련 API")
+public interface WatchApiSpecification {
+
+    @Operation(
+            summary = "시청 내역 조회",
+            description = "로그인한 회원의 시청내역 조회"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "조회 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = WatchResponseDto.class)
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @Parameter(hidden = true)
+    ResponseEntity<?> getWatchHistory(@AuthenticationPrincipal UserDetails userDetails);
+
+    @Operation(
+            summary = "시청 내역 조회",
+            description = "로그인한 회원의 시청내역 조회"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "조회 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = WatchResponseDto.class)
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "추가할 시청 정보를 담은 JSON",
+            required    = true,
+            content     = @Content(
+                    mediaType = "application/json",
+                    schema    = @Schema(
+                            type        = "object",
+                            description = "추가할 시청 정보 예시",
+                            example     = "{\"streamNo\":\"1\", \"watchTime\":\"2025-01-01T12:00:00\"}"
+                    )
+            )
+    )
+    ResponseEntity<?> insertWatchHistory(@AuthenticationPrincipal UserDetails userDetails,
+                                         @RequestBody WatchRequestDto watchRequestDto);
+
+}
