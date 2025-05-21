8b6593211d2df049c818900f219f7d59ce9fd20b
Kim Daecheol <kdc9619@naver.com>
2025-05-21 18:33:10 +0900

    Feat: 응답 description 인터페이스 구현 #38

---
+++ src/main/java/com/ssginc/showpingrefactoring/common/swagger/ApiResponseDescriptions.java
@@ -0,0 +1,8 @@
+package com.ssginc.showpingrefactoring.common.swagger;
+
+public interface ApiResponseDescriptions {
+    String SERVER_ERROR = "내부 서버 오류";
+    String NOT_FOUND = "리소스를 찾을 수 없음";
+    String UNAUTHORIZED = "인증 정보가 없거나 유효하지 않음";
+    String FORBIDDEN = "접근 권한이 없음";
+}
