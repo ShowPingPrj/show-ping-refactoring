7dd1301eb50fe8c8ee75b2897660db4b7be02b82
Kim Daecheol <kdc9619@naver.com>
2025-05-22 21:55:37 +0900

    Refactor: API 응답 인터페이스를 클래스로 변경
    - 상수 인터페이스 작성시 발생하는 안티(Anti)패턴을 지양하기 위함

--- src/main/java/com/ssginc/showpingrefactoring/common/swagger/ApiResponseDescriptions.java
+++ src/main/java/com/ssginc/showpingrefactoring/common/swagger/ApiResponseDescriptions.java
@@ -1,8 +1,11 @@
 package com.ssginc.showpingrefactoring.common.swagger;
 
-public interface ApiResponseDescriptions {
-    String SERVER_ERROR = "내부 서버 오류";
-    String NOT_FOUND = "리소스를 찾을 수 없음";
-    String UNAUTHORIZED = "인증 정보가 없거나 유효하지 않음";
-    String FORBIDDEN = "접근 권한이 없음";
+public final class ApiResponseDescriptions {
+
+    private ApiResponseDescriptions(){}
+
+    public static final String SERVER_ERROR = "내부 서버 오류";
+    public static final String NOT_FOUND = "리소스를 찾을 수 없음";
+    public static final String UNAUTHORIZED = "인증 정보가 없거나 유효하지 않음";
+    public static final String FORBIDDEN = "접근 권한이 없음";
 }
