b723c260934192c89e9118dd12d06502e9d8024d
Kim Daecheol <kdc9619@naver.com>
2025-05-27 19:19:30 +0900

    Chore: 불필요한 import 삭제

--- src/main/java/com/ssginc/showpingrefactoring/domain/stream/controller/VodApiController.java
+++ src/main/java/com/ssginc/showpingrefactoring/domain/stream/controller/VodApiController.java
@@ -11,7 +11,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
