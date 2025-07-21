8da6c5f191bcd2575ea83af9040d51d391639408
Kim Daecheol <kdc9619@naver.com>
2025-07-21 16:29:29 +0900

    Refactor: VOD 목록 접근권한 수정
    - 로그인한 사용자(관리자.구매자) → 전체 권한으로 변경

--- src/main/java/com/ssginc/showpingrefactoring/common/config/SecurityConfig.java
+++ src/main/java/com/ssginc/showpingrefactoring/common/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
                                         "/favicon.ico", "/api/auth/**",  "/api/member/check-duplicate", "/api/member/register",
                                         "/api/member/send-code/**", "/api/member/check-email-duplicate", "/api/member/check-phone-duplicate", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html","/api/batch/**", "/api/live/standby"
                                         ,"/api/live/product/list", "/api/live/onair", "/api/live/live-info", "/api/live/active", "/stream/watch/**", "/stream/list/**", "/watch/**",
-                                        "/api/watch/insert","/product/product_list","/product/product_list/**","/product/product_detail/**","/record", "/live"
+                                        "/api/watch/insert","/product/product_list","/product/product_list/**","/product/product_detail/**","/record", "/live", "/api/vod/list/**"
                                 ).permitAll()
                                 // ADMIN 전용 URL (두 코드 블록의 ADMIN 관련 URL 병합)
                                 .requestMatchers("/admin/**","/api/live/stop", "/api/live/start", "/api/live/register", "/api/report/updateStatus", "/api/report/register",
@@ -70,7 +70,7 @@ public class SecurityConfig {
                                 // USER 전용 URL (두 코드 블록의 USER 관련 URL 병합)
                                 .requestMatchers(
                                         "/user/**","/api/carts/**", "/api/payments/**", "/api/orders/**", "/api/watch/history/**", "/api/hls/**", "/api/payments/verify",
-                                        "/api/payments/complete", "/api/vod/list/**", "/api/chat/**","/watch/history", "/watch/history/**","/api/watch/history/list","/api/watch/history/list/**", "/cart/**", "/product/product_cart", "/payment/**",
+                                        "/api/payments/complete", "/api/chat/**","/watch/history", "/watch/history/**","/api/watch/history/list","/api/watch/history/list/**", "/cart/**", "/product/product_cart", "/payment/**",
                                         "/product/product_payment", "/success/**", "/payment/success/**"
                                 ).hasAnyRole("USER", "ADMIN")
                                 .anyRequest().authenticated()
